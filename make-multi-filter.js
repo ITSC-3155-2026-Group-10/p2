8ea674aeb0457487bcacde44e121871a372cbad8
Julius Dames <jdames1@charlotte.edu>
2026-02-08 00:56:48 -0500

    Create make-multi-filter.js

---
+++ make-multi-filter.js
@@ -0,0 +1,5 @@
+
+function MakeMultiFilter(originalArray) {
+  //TODO: arrayFilterer internally keeps track of a notion called currentArray
+  //arrayFilterer(filterCriteria, callback)
+  return arrayFilterer
